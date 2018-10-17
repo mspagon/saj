40b95e42a6d2c05650ac645d33c3170de7ad4407
mspagon <mspagon@gmail.com>
2018-10-17 05:47:48 -0500

    successful deploy to app engine

--- saj.py
+++ saj.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask import request
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Please Login!"
+    return "Add two numbers by entering using the add endpoint<BR><BR>Example:<BR>michaelspagon.com/add/55.15/22/"
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    return "{} + {} = {}".format(num1, num2, num1 + num2)
 
 
 if __name__ == ("__main__"):
