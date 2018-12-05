201f3694323f4e475bf73eb14e126eff36a19f1c
mspagon <mspagon@gmail.com>
2018-12-04 20:13:34 -0600

    blah

--- saj.py
+++ saj.py
@@ -1,23 +1,18 @@
 from flask import Flask
 from flask import render_template
 
+from models import models
+
+import json
+
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route("/<name>")
-def index(name="User"):
-    return render_template("index.html", name=name)
-
-
-@app.route('/add/<int:num1>/<int:num2>')
-@app.route('/add/<float:num1>/<float:num2>')
-@app.route('/add/<int:num1>/<float:num2>')
-@app.route('/add/<float:num1>/<int:num2>')
-def add(num1, num2):
-    context = {'num1': num1, 'num2': num2}
-    return render_template("add.html", **context)
+def index():
+    return render_template("index.html")
 
 
 if __name__ == ("__main__"):
+    models.initialize()
     app.run()
